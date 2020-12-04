a65f9720d7180780f75c69f7f5423ebd8f85231d
Timothy Yoon <timsyoon@gmail.com>
2020-12-03 18:36:23 -0800

    Create nurikabe.js and define the 2D array for the puzzle

---
+++ js/nurikabe.js
@@ -0,0 +1,8 @@
+// Hard code a single 5x5 grid
+var grid = [
+    [3, "w", "w", "w", "w"],
+    ["w", "w", 2, "w", 2],
+    ["w", "w", "w", "w", "w"],
+    ["w", "w", 1, "w", 1],
+    [2, "w", "w", "w", "w"]
+];
